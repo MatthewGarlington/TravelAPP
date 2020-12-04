339f7db02639c8c9d84156097bc7e090a60d0d5a
Matthew Garlington <SBMatthewG@gmail.com>
2020-12-04 14:44:58 -0500

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Matthew Garlington on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
