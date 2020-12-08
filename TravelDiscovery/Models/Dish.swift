57acfe71f9a38dc778d0d25b81a00b0cff8a9558
Matthew Garlington <SBMatthewG@gmail.com>
2020-12-08 15:27:36 -0500

    This is a View Model to fetch The JSON array of dish from an API

--- TravelDiscovery/Models/Dish.swift
+++ TravelDiscovery/Models/Dish.swift
@@ -7,14 +7,9 @@
 
 import SwiftUI
 
-struct Dish: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct Dish: Decodable, Hashable {
+    let name, price, photo: String
+    let numPhotos: Int
 }
 
-struct Dish_Previews: PreviewProvider {
-    static var previews: some View {
-        Dish()
-    }
-}
+
