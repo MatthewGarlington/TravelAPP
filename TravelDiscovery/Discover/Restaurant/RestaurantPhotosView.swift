0633fb34e6606af61f93db1491c4100ee3308806
Matthew Garlington <SBMatthewG@gmail.com>
2020-12-08 17:27:11 -0500

    This is a Deatil View of Photo's from the Resturants menu with comments in List style and a Lazy Grid to toggle between using a custom Picker style

---
+++ TravelDiscovery/Discover/Restaurant/RestaurantPhotosView.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantPhotosView.swift
+//  TravelDiscovery
+//
+//  Created by Matthew Garlington on 12/8/20.
+//
+
+import SwiftUI
+
+struct RestaurantPhotosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RestaurantPhotosView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestaurantPhotosView()
+    }
+}
