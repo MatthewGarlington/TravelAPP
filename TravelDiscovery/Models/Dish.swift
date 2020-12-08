99fa02bfc76d451798113b4240d332afe3c46d8f
Matthew Garlington <SBMatthewG@gmail.com>
2020-12-08 15:23:21 -0500

    Refactored Code for dish Json Object

---
+++ TravelDiscovery/Models/Dish.swift
@@ -0,0 +1,20 @@
+//
+//  Dish.swift
+//  TravelDiscovery
+//
+//  Created by Matthew Garlington on 12/8/20.
+//
+
+import SwiftUI
+
+struct Dish: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Dish_Previews: PreviewProvider {
+    static var previews: some View {
+        Dish()
+    }
+}
---
+++ TravelDiscovery/Models/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  TravelDiscovery
+//
+//  Created by Matthew Garlington on 12/8/20.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
