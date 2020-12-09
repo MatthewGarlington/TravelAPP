487e2c2c6cf638a506fb96d50449c4c0ec5592d0
Matthew Garlington <SBMatthewG@gmail.com>
2020-12-08 22:44:07 -0500

    This is a User view to fetch user data in a JSON format from an API. Using SwiftUI and DEcoable protocols to decode the json into a User Profile image with dummy data

---
+++ TravelDiscovery/Discover/User/UserDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  UserDetailsView.swift
+//  TravelDiscovery
+//
+//  Created by Matthew Garlington on 12/8/20.
+//
+
+import SwiftUI
+
+struct UserDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserDetailsView()
+    }
+}
